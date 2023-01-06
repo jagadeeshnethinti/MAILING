bedb448bcf2e5af7edda5f62293e4ff1b4e88d26
jagadeeshnethinti <109734275+jagadeeshnethinti@users.noreply.github.com>
2023-01-06 22:12:05 +0530

    Add files via upload

---
+++ mailing.py
@@ -0,0 +1,7 @@
+email=input()
+if email.endswith("@gmail.com"):
+    print('you are using google account')
+if email.endswith("@yahoo.com"):
+    print('you are using yahoo accont')
+if email.endswith("@adc.aditya.ac.in"):
+    print('you are using aditya education institute accout')
